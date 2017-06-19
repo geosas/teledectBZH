4b2335483e9af077c621e81b1a3532e2e909a88c
Donatien Dallery <dallery.donatien@gmail.com>
2017-06-19 14:54:33 +0200

    maj

--- Executable.sh
+++ Executable.sh
@@ -1,4 +1,4 @@
-python '/home/donatien/ProjectGeoBretagne/DownloadMODIS.py' -path '/home/donatien/ProjectGeoBretagne/DatasEffacer/' -fdate 2017-05-01 -ldate 2017-05-10
+python '/home/donatien/ProjectGeoBretagne/Scripts/DownloadMODIS.py' -path '/home/donatien/ProjectGeoBretagne/Datas/' -fdate 2017-05-01 -ldate 2017-05-10
 
-python '/home/donatien/ProjectGeoBretagne/TreatmentEF.py' -d '/home/donatien/ProjectGeoBretagne/Datas' -out '/home/donatien/ProjectGeoBretagne/Datas/Output'
+python '/home/donatien/ProjectGeoBretagne/Scripts/TreatmentEF.py' -d '/home/donatien/ProjectGeoBretagne/Datas' -out '/home/donatien/ProjectGeoBretagne/Datas/Output'
 
