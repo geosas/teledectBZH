c535d756763f9984a0f3e904dabbab3e5bff86ee
Donatien Dallery <dallery.donatien@gmail.com>
2017-07-07 17:43:00 +0200

    doc metadonnees

--- Executable.sh
+++ Executable.sh
@@ -2,3 +2,5 @@ python '/home/donatien/ProjectGeoBretagne/Scripts/DownloadMODIS.py' -path '/home
 
 python '/home/donatien/ProjectGeoBretagne/Scripts/TreatmentEF.py' -d '/home/donatien/ProjectGeoBretagne/Datas' -out '/home/donatien/ProjectGeoBretagne/Datas/Output'
 
+python /home/teledectBZH/Scripts/PublishGeoserver.py -url http://geoxxx.agrocampus-ouest.fr/geoserver/rest/workspaces/ -wspace geouest -store EF_teledectBZH -datadir /home/data/gi2016/teledectBZH -co /home/teledectBZH/.logPassGeoserver
+
