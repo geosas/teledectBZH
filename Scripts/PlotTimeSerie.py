e3b20223fbe6e722182ce5687a0d81729527469e
Donatien Dallery <dallery.donatien@gmail.com>
2017-07-28 12:21:56 +0200

    plot temporal series

--- Scripts/PlotTimeSerie.py
+++ Scripts/PlotTimeSerie.py
@@ -8,11 +8,12 @@ import os
 import sys
 import numpy as np
 import argparse
-import glob
+import ogr
+import osr
 import gdal
 import datetime
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 def OpenRaster(inRaster, band):
@@ -38,15 +39,15 @@ def OpenRaster(inRaster, band):
     rasterBand = raster.GetRasterBand(band)
     
     # property of raster
-    #projection = raster.GetProjectionRef()
-    #transform = raster.GetGeoTransform()
+    projection = raster.GetProjectionRef()
+    transform = raster.GetGeoTransform()
     xsize = raster.RasterXSize
     ysize = raster.RasterYSize
     
     # convert raster to an array
     datas = rasterBand.ReadAsArray()
     
-    return datas, xsize, ysize, #projection, transform
+    return datas, xsize, ysize, projection, transform
     
 def listRasters(datas):
     """
@@ -54,36 +55,108 @@ def listRasters(datas):
 
     """
     lRasters = []
+    lDates = []
     for path, dossiers, rasters in os.walk(datas):
+        rasters.sort()
         for raster in rasters :
             # extrait la date du fichier XXX_YYYYMMDD.tif
             date = raster[-12:-4]
             try:
-                datetime.datetime.strptime(date, "%Y%m%d")
+                dateConvert = datetime.datetime.strptime(date, "%Y%m%d")
+                lDates.append(dateConvert)
             except ValueError :
-                print "L'image %s n'est pas au format XXX_YYYYMMDD.tif" % (raster)
-                sys.exit()
+                sys.exit("L'image %s n'est pas au format XXX_YYYYMMDD.tif" % (raster))
             lRasters.append(path+"/"+raster)
-    lRasters.sort()
-    return lRasters
-            dstack
-def main(datas, out, roi):
+    return lRasters, lDates
+    
+def extractArea(shape, nodata, xsize, ysize, projection, transform):
+    """
+    """
+    # Ouverture du shapefile
+    driver = ogr.GetDriverByName('ESRI Shapefile')
+    if not os.path.exists(shape):
+        sys.exit("Le fichier shape indique n'existe pas")        
+    dtSource = driver.Open(shape, 0)    
+    if dtSource is None:
+        sys.exit("Impossible d'ouvrir le shapefile")    
+    layer = dtSource.GetLayer()
+    feature = layer.GetNextFeature()
+    
+    # Variables du raster
+    xOrigin = transform[0]
+    yOrigin = transform[3]
+    pixelWidth = transform[1]
+    pixelHeight = transform[5]
+    
+    # Etendue de l'entite du shapefile
+    geom = feature.GetGeometryRef()
+    if (geom.GetGeometryName() == 'MULTIPOLYGON'):
+        count = 0
+        pointsX = []; pointsY = []
+        for polygon in geom:
+            geomInner = geom.GetGeometryRef(count)
+            ring = geomInner.GetGeometryRef(0)
+            numpoints = ring.GetPointCount()
+            for p in range(numpoints):
+                lon, lat, z = ring.GetPoint(p)
+                pointsX.append(lon)
+                pointsY.append(lat)
+            count += 1
+    elif (geom.GetGeometryName() == 'POLYGON'):
+        ring = geom.GetGeometryRef(0)
+        numpoints = ring.GetPointCount()
+        pointsX = []; pointsY = []
+        for p in range(numpoints):
+            lon, lat, z = ring.GetPoint(p)
+            pointsX.append(lon)
+            pointsY.append(lat)
+
+    else:
+        sys.exit("L'entite n'est pas un polygon")
+        
+    xmin = min(pointsX)
+    xmax = max(pointsX)
+    ymin = min(pointsY)
+    ymax = max(pointsY)
+
+    # Calcul l'emprise de l'entite selon la taille du raster
+    xoff = int((xmin - xOrigin)/pixelWidth)
+    yoff = int((yOrigin - ymax)/pixelWidth)
+    xcount = int((xmax - xmin)/pixelWidth)+1
+    ycount = int((ymax - ymin)/pixelWidth)+1
+ 
+    return yoff, ycount, xoff, xcount
+
+    
+def main(datas, out, roi, nan):
     """
     """
     # Liste tous les fichiers rasters et teste s'ils ont tous une information
     # temporelle dans leur nom
-    lRasters = listRasters(datas)
+    lRasters, lDates = listRasters(datas)
     
-    # Initialise un tableau en 3 dimensions selon la dimension d'une image et
-    # le nombre d'images
-    data, xsize, ysize = OpenRaster(lRasters[0],1)
-    del data
-    matrix = np.empty((xsize,ysize,len(lRasters)))
+    # Initialise la combinaison de tous les rasters dans un seul array
+    datas, xsize, ysize, projection, transform = OpenRaster(lRasters[0],1)
     
     # Ajoute les donnees de chacune des images dans la matrice en 3D
-    for raster in lRasters:
-        data = OpenRaster(raster, 1)
+    for raster in lRasters[1:]:
+        data, xsize, ysize, projection, transform = OpenRaster(raster, 1)
+        datas = np.dstack((datas, data))
         
+    # Calcule l'emprise de l'entite par rapport au raster
+    yoff, ycount, xoff, xcount = extractArea(roi, nan, xsize, ysize, projection, transform)
+    
+    # Change les valeurs des nodata en nan
+    datas[np.where(datas==-999)]=np.nan
+    
+    # Calcule la valeur moyenne pour chaque date
+    y = [np.nanmean(datas[yoff:yoff+ycount, xoff: xoff+xcount,i]) for i in range(len(lRasters))]
+    #for i in range(len(lRasters)):
+        #np.nanmean(datas[yoff:yoff+ycount, xoff: xoff+xcount,i])
+    plt.plot(lDates, y, marker='o')
+    plt.gcf().autofmt_xdate()
+    plt.show()
+    
     
 if __name__ == "__main__":
     if len(sys.argv) == 1:
@@ -107,12 +180,15 @@ if __name__ == "__main__":
         parser.add_argument("-roi", dest="roi", action="store", 
                             help="Shapefile de la zone d'interet")
                             
+        parser.add_argument("-ndata", dest="nan", action="store", 
+                            help="Valeur du nodata")
+                            
         args = parser.parse_args()
         
     np.seterr(divide='ignore', invalid='ignore')
     if not os.path.exists(args.out):
         os.mkdir(args.out)
         
-    main(args.datas, args.out, args.roi)
+    main(args.datas, args.out, args.roi, args.nan)
     
     print "\nGraphique genere"
